bd7f5213fb650f204bab3b3b35b452f3cffafde4
Ze Jin <zj123@ic.ac.uk>
2023-10-09 23:20:15 +0100

    Added the comments

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+/*Create event listener which responds to a click event*/
 document.querySelector("#button1").addEventListener("click", function () {
     document.getElementById("chart1").style.visibility = "visible";
     document.getElementById("button1").innerHTML = "Hide chart"
